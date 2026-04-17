18c324182da1f5a09877635c9861778327a07f6f
lutzpeschlow <lutz.peschlow@gmail.com>
2026-04-17 15:46:57 +0200

    comment and simplify

--- main.go
+++ main.go
@@ -17,49 +17,74 @@ type Data struct {
 	Error  string
 }
 
+// ======================================================================================
+// main
+//
+// main function
+// - load template
+// - register two routines
+// - start server
+//
+// ======================================================================================
 func main() {
+	// load html template from index.html
+	// read the file with parsefiles and create a template object
+	// must valid template
 	tpl := template.Must(template.ParseFiles("templates/index.html"))
-
+	// register a route
+	// with / start function, w is the response, r is the request from browser
+	// anonymous function, will defined directly
+	// tpl.execute is rendering template and it will send to browser
+	// data object as empty object
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_ = tpl.Execute(w, Data{})
 	})
-
+	// register second route, responsible for final content
+	// send formular per POST
+	// if no POST, e.g. GET no response and return
 	http.HandleFunc("/calculate", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			http.Redirect(w, r, "/", http.StatusSeeOther)
 			return
 		}
-
+		// read input lines
+		// convert into integer
 		num1Str := r.FormValue("num1")
 		num2Str := r.FormValue("num2")
-
 		num1, err := strconv.Atoi(num1Str)
 		if err != nil {
-			_ = tpl.Execute(w, Data{Error: "Erste Zahl ungültig!"})
+			_ = tpl.Execute(w, Data{Error: "invalid number"})
 			return
 		}
-
 		num2, err := strconv.Atoi(num2Str)
 		if err != nil {
-			_ = tpl.Execute(w, Data{Error: "Zweite Zahl ungültig!"})
+			_ = tpl.Execute(w, Data{Error: "invalid number"})
 			return
 		}
-
+		// processing numbers in separate function
 		result := processIntegers(num1, num2)
+		// fill template with numbers
 		_ = tpl.Execute(w, Data{Num1: num1, Num2: num2, Result: result})
 	})
-
+	// adress of server
 	fmt.Println("Server: http://localhost:8080")
 	fmt.Println("Browser...")
-
+	// start go routine
+	// parallel running without blocking main
+	// wait for three seconds, time for webserver before opening browser with adress
 	go func() {
 		time.Sleep(3 * time.Second)
 		openBrowser("http://localhost:8080")
 	}()
-
+	// webserver start at port 8080
 	_ = http.ListenAndServe(":8080", nil)
 }
 
+// ======================================================================================
+//
+// openBrowser
+//
+// ======================================================================================
 func openBrowser(url string) error {
 	switch runtime.GOOS {
 	case "linux":
@@ -71,11 +96,13 @@ func openBrowser(url string) error {
 	}
 }
 
+// ======================================================================================
+//
+// processIntegers
+//
+// ======================================================================================
 func processIntegers(a, b int) string {
 	fmt.Println("data processing ...")
 	fmt.Println(a, b)
-	return fmt.Sprintf(
-		"Ergebnisse:\n• Summe: %d + %d = %d\n• Produkt: %d × %d = %d\n• Differenz: %d - %d = %d\n• Quotient: %d ÷ %d = %d",
-		a, b, a+b, a, b, a*b, a, b, a-b, a, b, a/b,
-	)
+	return "result"
 }
